998c9a785a9d2794f9d5552fb86b6d4d110e5ccd
tommaso.lazzaretti <tommaso.lazzaretti@gmail.com>
2022-10-14 18:06:12 +0200

    simple crud (without payload)

--- index.js
+++ index.js
@@ -13,15 +13,99 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
+const fluent_json_schema_1 = __importDefault(require("fluent-json-schema"));
+const axios = require('axios');
 const server = (0, fastify_1.default)();
-server.get('/ping', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
-    return 'pong\n';
+const AXIOS_HEADERS = {
+    'app-id': '634929d545ebdf36e1a19608'
+};
+server.get('/status', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    return {
+        status: 'ok'
+    };
 }));
 server.get('/auth', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
     const { username, password } = request.query;
     const customerHeader = request.headers['h-Custom'];
     // do something with request data
     return `logged in!`;
+}));
+const bodyJsonSchema = fluent_json_schema_1.default.object()
+    .additionalProperties(false)
+    .prop('title', fluent_json_schema_1.default.string())
+    .prop('author', fluent_json_schema_1.default.string());
+// Note that there is no need to call `.valueOf()`!
+const schema = {
+    body: bodyJsonSchema,
+    /*response: {
+      200: S.object().prop('status', S.string()))
+    },*/
+};
+/**
+ * getAll posts *paginated
+ */
+server.get('/getPost', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    yield axios.get('https://dummyapi.io/data/v1/post', { headers: AXIOS_HEADERS })
+        .then((res) => {
+        reply.send(res.data);
+    })
+        .catch((err) => {
+        console.log('Error: ', err.message);
+    });
+    console.log("return nooooooooow");
+    return reply;
+}));
+/**
+ * getAll posts *by user
+ */
+server.get('/getPostByUser', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    yield axios.get('https://dummyapi.io/data/v1/user/:id/post', { headers: AXIOS_HEADERS })
+        .then((res) => {
+        reply.send(res.data);
+    })
+        .catch((err) => {
+        console.log('Error: ', err.message);
+    });
+    return reply;
+}));
+/**
+ * create a new Post
+ */
+server.post('/createPost', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    yield axios.post('https://dummyapi.io/data/v1/post', { headers: AXIOS_HEADERS })
+        .then((res) => {
+        reply.send(res.data);
+    })
+        .catch((err) => {
+        console.log('Error: ', err.message);
+    });
+    return reply;
+}));
+/**
+ * update an existing post
+ */
+server.post('/updatePost', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    yield axios.put('https://dummyapi.io/data/v1/post', { headers: AXIOS_HEADERS })
+        .then((res) => {
+        reply.send(res.data);
+    })
+        .catch((err) => {
+        console.log('Error: ', err.message);
+    });
+    return reply;
+}));
+/**
+ * delete an existing post
+ */
+server.post('/deletePost', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
+    yield axios.delete('https://dummyapi.io/data/v1/post', { headers: AXIOS_HEADERS })
+        .then((res) => {
+        reply.send(res.data);
+    })
+        .catch((err) => {
+        console.log('Error: ', err.message);
+    });
+    return reply;
 }));
 server.listen({ port: 8080 }, (err, address) => {
     if (err) {
--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify';
+import S from 'fluent-json-schema';
 import {IHeaders, IUser} from "./global/interfaces/Auth";
+import {AxiosResponse} from "axios";
 
+const axios = require('axios');
 const server = fastify()
 
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
-})
+const AXIOS_HEADERS: any = {
+    'app-id': '634929d545ebdf36e1a19608'
+};
 
 server.get('/status', async (request, reply) => {
     return {
@@ -17,14 +20,89 @@ server.get<{
     Querystring: IUser,
     Headers: IHeaders
 }>('/auth', async (request, reply) => {
-    const { username, password } = request.query
+    const {username, password} = request.query
     const customerHeader = request.headers['h-Custom']
     // do something with request data
 
     return `logged in!`
 })
 
-server.listen({ port: 8080 }, (err, address) => {
+/**
+ * getAll posts *paginated
+ */
+server.get('/getPost', async (request, reply) => {
+    await axios.get('https://dummyapi.io/data/v1/post', {headers: AXIOS_HEADERS})
+        .then((res: AxiosResponse<any>) => {
+            reply.send(res.data);
+        })
+        .catch((err: { message: string; }) => {
+            console.log('Error: ', err.message);
+        });
+
+    return reply;
+})
+
+/**
+ * getAll posts *by user
+ */
+server.get('/getPostByUser', async (request, reply) => {
+    await axios.get('https://dummyapi.io/data/v1/user/:id/post', {headers: AXIOS_HEADERS})
+        .then((res: AxiosResponse<any>) => {
+            reply.send(res.data);
+        })
+        .catch((err: { message: string; }) => {
+            console.log('Error: ', err.message);
+        });
+
+    return reply;
+})
+
+/**
+ * create a new Post
+ */
+server.post('/createPost', async (request, reply) => {
+    await axios.post('https://dummyapi.io/data/v1/post/create', {headers: AXIOS_HEADERS})
+        .then((res: AxiosResponse<any>) => {
+            reply.send(res.data);
+        })
+        .catch((err: { message: string; }) => {
+            console.log('Error: ', err.message);
+        });
+
+    return reply;
+})
+
+/**
+ * update an existing post
+ */
+server.post('/updatePost', async (request, reply) => {
+    await axios.put('https://dummyapi.io/data/v1/post/:id', {headers: AXIOS_HEADERS})
+        .then((res: AxiosResponse<any>) => {
+            reply.send(res.data);
+        })
+        .catch((err: { message: string; }) => {
+            console.log('Error: ', err.message);
+        });
+
+    return reply;
+})
+
+/**
+ * delete an existing post
+ */
+server.post('/deletePost', async (request, reply) => {
+    await axios.delete('https://dummyapi.io/data/v1/post/:id', {headers: AXIOS_HEADERS})
+        .then((res: AxiosResponse<any>) => {
+            reply.send(res.data);
+        })
+        .catch((err: { message: string; }) => {
+            console.log('Error: ', err.message);
+        });
+
+    return reply;
+})
+
+server.listen({port: 8080}, (err, address) => {
     if (err) {
         console.error(err)
         process.exit(1)
